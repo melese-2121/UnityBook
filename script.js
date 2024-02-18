8cb746a961e79614a0bf1791036471ca28dcb1ee
melese <meleseayen1999@gmail.com>
2024-02-18 14:22:31 +0300

    Story finished

---
+++ script.js
@@ -0,0 +1,25 @@
+const initSlider = () => {
+  const imageList = document.querySelector(".shared-story-cont .img-list");
+  const slideButtons = document.querySelectorAll(
+    ".shared-story-cont .slide-button"
+  );
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = imageList.clientWidth * direction;
+      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+  });
+
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display =
+      imageList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display =
+      imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  imageList.addEventListener("scroll", () => handleSlideButtons());
+};
+
+window.addEventListener("load", initSlider);
